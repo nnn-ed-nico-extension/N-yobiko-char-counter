2db43693e1bd4619b6c6b771c4a48aa350b240a4
tsutoringo <33830878+tsutoringo@users.noreply.github.com>
2021-05-02 11:53:41 +0900

    popup実装

--- src/popup/main.js
+++ src/popup/main.js
@@ -1,6 +1,14 @@
 import Vue from 'vue';
 import App from './App.vue';
 
+import { library } from '@fortawesome/fontawesome-svg-core';
+import { faCodeBranch } from '@fortawesome/free-solid-svg-icons';
+import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome';
+
+library.add(faCodeBranch);
+
+Vue.component('fa-icon', FontAwesomeIcon);
+
 /* eslint-disable no-new */
 new Vue({
   el: '#app',
