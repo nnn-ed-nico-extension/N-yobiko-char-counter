7679e9e5003e17fb8ba8965bb47cd31cdb93086b
tsutoringo <33830878+tsutoringo@users.noreply.github.com>
2021-06-01 22:36:27 +0900

    Update for firefox

--- src/content-scripts/contentScript.js
+++ src/content-scripts/contentScript.js
@@ -1,7 +1,8 @@
 import CounterInput from './CounterInput';
 
-const fields = document.querySelectorAll(".section-item .exercise-item.type-descriptive");
-
-for (let key=0;fields.length>key;key++) {
-	new CounterInput(fields[key]);
-}
+setTimeout(() => {
+	const fields = document.querySelectorAll(".section-item .exercise-item.type-descriptive");
+	for (let key=0;fields.length>key;key++) {
+		new CounterInput(fields[key]);
+	}
+}, 500);
