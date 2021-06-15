8ea9d17cd8a320f0b3ab4808451f966c5f426852
tsutoringo <33830878+tsutoringo@users.noreply.github.com>
2021-06-16 02:05:08 +0900

    Fontawesomeのコード追加

--- src/popup/main.js
+++ src/popup/main.js
@@ -3,6 +3,14 @@ import App from './App.vue';
 import { Octicon } from 'octicons-vue';
 import router from './router';
 
+import { library } from '@fortawesome/fontawesome-svg-core'
+import { faArrowLeft, faInfoCircle } from '@fortawesome/free-solid-svg-icons'
+import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
+
+library.add(faArrowLeft, faInfoCircle);
+
+Vue.component('fa-icon', FontAwesomeIcon)
+
 
 Vue.component('octicon', Octicon);
 
